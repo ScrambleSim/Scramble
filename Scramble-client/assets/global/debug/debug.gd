abbe21f582a238ebb086d291dba90676f730c50f
Tom Langwaldt <t.langwaldt@gmail.com>
2020-04-12 17:34:41 +0200

    Fix comment formatting

--- Scramble-client/assets/global/debug/debug.gd
+++ Scramble-client/assets/global/debug/debug.gd
@@ -20,7 +20,7 @@ func _process(delta):
         lines[i].time -= delta
     
     if(len(lines) > 0 || removed_line):
-        update() #Calls _draw
+        update() # Calls _draw
         removed_line = false
 
 func _draw():
@@ -29,16 +29,16 @@ func _draw():
         var screen_point_start = cam.unproject_position(lines[i].start)
         var screen_point_end = cam.unproject_position(lines[i].end)
         
-        #Dont draw line if either start or end is considered behind the camera
-        #this causes the line to not be drawn sometimes but avoids a bug where the
-        #line is drawn incorrectly
+        # Dont draw line if either start or end is considered behind the camera
+        # this causes the line to not be drawn sometimes but avoids a bug where the
+        # line is drawn incorrectly
         if(cam.is_position_behind(lines[i].start) ||
             cam.is_position_behind(lines[i].end)):
             continue
         
         draw_debug_line(screen_point_start, screen_point_end, lines[i].line_color)
     
-    #Remove lines that have timed out
+    # Remove lines that have timed out
     var i = lines.size() - 1
     while (i >= 0):
         if(lines[i].time < 0.0):
@@ -53,13 +53,13 @@ func draw_debug_ray(start, ray, line_color, time = 0.0):
     lines.append(Line.new(start, start + ray, line_color, time))
 
 func draw_cube(center, half_extents, line_color, time = 0.0):
-    #Start at the 'top left'
+    # Start at the 'top left'
     var line_point_start = center
     line_point_start.x -= half_extents
     line_point_start.y += half_extents
     line_point_start.z -= half_extents
     
-    #Draw top square
+    # Draw top square
     var line_point_end = line_point_start + Vector3(0, 0, half_extents * 2.0)
     draw_debug_line(line_point_start, line_point_end, line_color, time);
     line_point_start = line_point_end
@@ -72,7 +72,7 @@ func draw_cube(center, half_extents, line_color, time = 0.0):
     line_point_end = line_point_start + Vector3(-half_extents * 2.0, 0, 0)
     draw_debug_line(line_point_start, line_point_end, line_color, time);
     
-    #Draw bottom square
+    # Draw bottom square
     line_point_start = line_point_end + Vector3(0, -half_extents * 2.0, 0)
     line_point_end = line_point_start + Vector3(0, 0, half_extents * 2.0)
     draw_debug_line(line_point_start, line_point_end, line_color, time);
@@ -86,7 +86,7 @@ func draw_cube(center, half_extents, line_color, time = 0.0):
     line_point_end = line_point_start + Vector3(-half_extents * 2.0, 0, 0)
     draw_debug_line(line_point_start, line_point_end, line_color, time);
     
-    #Draw vertical lines
+    # Draw vertical lines
     line_point_start = line_point_end
     draw_debug_ray(line_point_start, Vector3(0, half_extents * 2.0, 0), line_color, time)
     line_point_start += Vector3(0, 0, half_extents * 2.0)
