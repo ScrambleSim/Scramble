863ac00e319553500208288b5ebb5a52f766fcea
Tom Langwaldt <t.langwaldt@gmail.com>
2020-04-13 10:12:27 +0200

    Add all pilot animations to Scramble

--- Scramble-client/assets/entities/characters/pilot/scripts/player_control.gd
+++ Scramble-client/assets/entities/characters/pilot/scripts/player_control.gd
@@ -19,39 +19,39 @@ extends Node
 const MAX_MOVEMENT_SPEED = 5
 
 func _ready():
-	if not get_parent().is_posessed:
-		var pitch = $"../Pitch"
-		pitch.queue_free()
-		self.queue_free()
+    if not get_parent().is_posessed:
+        var pitch = $"../Pitch"
+        pitch.queue_free()
+        self.queue_free()
 
-	Input.set_mouse_mode(2)     # capture mouse
+    Input.set_mouse_mode(2)     # capture mouse
 
 
 func _process(delta):
-	self.move(delta)
-	self.look(delta)
+    self.move(delta)
+    self.look(delta)
 
 
 func move(_delta):
-	var p = self.get_parent()
-	var forward =  p.global_transform.basis.z * PropertyManager.player_move_FB * -1
-	var sideways = p.global_transform.basis.x * PropertyManager.player_move_LR * -1
-	var move_vel = (forward + sideways)
-	move_vel = move_vel.normalized()
-	p.move_and_slide(move_vel * MAX_MOVEMENT_SPEED, Vector3(0, 1, 0))
+    var p = self.get_parent()
+    var forward =  p.global_transform.basis.z * PropertyManager.player_move_FB * -1
+    var sideways = p.global_transform.basis.x * PropertyManager.player_move_LR * -1
+    var move_vel = (forward + sideways)
+    move_vel = move_vel.normalized()
+    p.move_and_slide(move_vel * MAX_MOVEMENT_SPEED, Vector3(0, 1, 0))
 
 
 func look(delta):
-	var pitch = $"../Pitch"
-	pitch.rotate_x(PropertyManager.look_vertical * delta * (-1.0))
-	var yaw = self.get_parent()
-	yaw.rotate_y(PropertyManager.look_horizontal * delta)
+    var pitch = $"../Pitch"
+    pitch.rotate_x(PropertyManager.look_vertical * delta * (-1.0))
+    var yaw = self.get_parent()
+    yaw.rotate_y(PropertyManager.look_horizontal * delta)
 
 
 # TODO remove if input manager supports mouse events
 func _input(event):
-	if event is InputEventMouseMotion:
-		var pitch = $"../Pitch"
-		pitch.rotate_x(event.relative.y * 0.003)
-		var yaw = self.get_parent()
-		yaw.rotate_y(event.relative.x * 0.003 * (-1.0))
+    if event is InputEventMouseMotion:
+        var pitch = $"../Pitch"
+        pitch.rotate_x(event.relative.y * 0.003)
+        var yaw = self.get_parent()
+        yaw.rotate_y(event.relative.x * 0.003 * (-1.0))
