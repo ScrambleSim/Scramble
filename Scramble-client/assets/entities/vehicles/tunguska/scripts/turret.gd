28851471a3a800a90e0da38a14c0a7c7091b7cb7
Tom Langwaldt <t.langwaldt@gmail.com>
2020-06-12 10:30:51 +0200

    Add axis locking export
    Using bitmasking

--- Scramble-client/assets/entities/vehicles/tunguska/scripts/turret.gd
+++ Scramble-client/assets/entities/vehicles/tunguska/scripts/turret.gd
@@ -1,5 +1,7 @@
 extends MeshInstance
 
+export(int, FLAGS, "X", "Y", "Z") var locked_axis = 0
+
 func _process(delta):
     var target_pos = get_node_or_null("/root/Scramble/World/Entities/fa18f")
     if not target_pos:
@@ -12,6 +14,19 @@ func _process(delta):
 
     self.rotation = current_rotation.linear_interpolate(final_rotation, delta * 2)
 
-    # Prevent pitching on x axis
-    self.rotation.x = 0
+    if is_bit_enabled(locked_axis, 0):
+        self.rotation.x = 0
+    if is_bit_enabled(locked_axis, 1):
+        self.rotation.y = 0
+    if is_bit_enabled(locked_axis, 2):
+        self.rotation.z = 0
+        
 
+    # requirements
+    # disable each axis individually
+    # interpolate yes no
+    # define target node
+    # define interpolation speed
+
+func is_bit_enabled(mask, index):
+    return mask & (1 << index) != 0
